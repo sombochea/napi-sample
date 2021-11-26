ef0cbc035cb8dcd635b765e8e192eaf44097194d
Sambo Chea <sombochea@cubetiqs.com>
2021-11-26 08:41:24 +0700

    Add fib fn

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,8 @@ fn init(mut exports: JsObject) -> Result<()> {
   exports.create_named_method("sleep", sleep)?;
 
   exports.create_named_method("say", say)?;
+
+  exports.create_named_method("fib", fib)?;
   Ok(())
 }
 
@@ -55,4 +57,19 @@ fn say(ctx: CallContext) -> Result<JsNumber> {
   let a: u32 = ctx.get::<JsNumber>(0)?.try_into()?;
   let b: u32 = ctx.get::<JsNumber>(1)?.try_into()?;
   ctx.env.create_uint32(a + b)
+}
+
+#[js_function(1)]
+fn fib(ctx: CallContext) -> Result<JsNumber> {
+  let n: i64 = ctx.get::<JsNumber>(0)?.try_into()?;
+  ctx.env.create_int64(fibonacci_native(n))
+}
+
+#[inline(always)]
+fn fibonacci_native(n: i64) -> i64 {
+  match n {
+    0 => 0,
+    1 | 2 => 1,
+    _ => fibonacci_native(n - 1) + fibonacci_native(n - 2),
+  }
 }
